26ed265cc5c69dc8d7c1ddd7ae1f3c78736d51c1
Daniel Lucena <daniel.lucena@olxbr.com>
2022-08-30 14:40:37 -0300

    add api url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=?';
+
+    fetch(url)
+    .then((result)=> result.json())
+    .then((json) => {
+      console.log(json);
+      setBooks(json);
+    })
+  },[])
+
   return (
     <div className="App">
       <h1>News Time App</h1>
